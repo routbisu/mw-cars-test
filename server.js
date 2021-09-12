daf33b945a650697ed325e6200fb406e5c7b6152
Biswa Rout <biswaranjan.rout1989@gmail.com>
2021-09-12 01:38:10 +0100

    Added image resizer

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
-"use strict";
+'use strict'
 
-const app = require("express")();
-const images = require("./src/images.json");
+const app = require('express')()
+const path = require('path')
+const sharp = require('sharp')
+const images = require('./src/images.json')
 
-const randomInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+// const randomInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
 
-app.get("/images", ({ query }, res) => {
-  const i = query.limit ? images.slice(0, parseInt(query.limit)) : images;
-  return res.status(200).json(i);
-});
+app.get('/images', ({ query }, res) => {
+  const i = query.limit ? images.slice(0, parseInt(query.limit)) : images
+  return res.status(200).json(i)
+})
 
-app.get(/^\/(car-images|avatars)\//, (req, res) => {
-  // If no extension, just serve the jpg file
-  res.status(200).sendFile(`public/${req.url}.jpg`, { root: __dirname });
-});
+app.get(/^\/(car-images|avatars)\//, async (req, res) => {
+  try {
+    // Resize images based on the param: size (used for both width and height)
+    const resizeOptions = isNaN(Number(req.query.size))
+      ? null
+      : {
+          width: Number(req.query.size),
+          height: Number(req.query.size),
+          fit: 'cover',
+        }
+    const imagePath = path.join(__dirname, `public/${req._parsedUrl.pathname}.jpg`)
+    const resizedImage = await sharp(imagePath).resize(resizeOptions).toBuffer()
+    res.contentType('image/jpeg')
+    res.status(200).end(resizedImage, 'binary')
+  } catch (error) {
+    res.status(400)
+  }
+})
 
 app.listen(5000, () => {
-  process.stdout.write("Server is available on http://localhost:5000/\n");
-});
+  process.stdout.write('Server is available on http://localhost:5000/\n')
+})
--- src/components/Gallery/Thumbnail/Thumbnail.js
+++ src/components/Gallery/Thumbnail/Thumbnail.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Thumbnail.scss'
 
+const getBgStyles = (url) => ({
+  background: `url(${url}?size=300), #000000`,
+  backgroundSize: 'cover',
+})
+
 const Thumbnail = ({ image: { url } }) => {
   return (
     <div className="thumbnail-container">
-      <div className="thumbnail-image" />
+      <button className="thumbnail-image" style={getBgStyles(url)}>
+        <div className="overlay"></div>
+      </button>
     </div>
   )
 }
